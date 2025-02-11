d47cfb4941ded002eb712eac9f20c8652e5b1424
pynhaXD <roma151205@mail.ru>
2025-02-11 16:39:25 +0700

    styles for app

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <router-view />
+  <div class="app-container">
+    <router-view />
+  </div>
 </template>
 
-<script>
-
-</script>
-
 <style>
-
+.app-container {
+  font-family: sans-serif;
+  margin: 0 auto;
+  padding: 16px;
+  max-width: 900px;
+  background-color: #fafafa;
+  min-height: 100vh;
+}
 </style>
